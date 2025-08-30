df0b96287523b5d03dbc6e5390558e2557518265
fuking12 <pqthumchinthaka105@gmail.com>
2025-08-30 18:27:59 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "zZ8R3LoC#zo2tBuuqDPo797uTpP4lHDG-xqzQorw8AK8UDU4JSRc", //put your session id
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
+MODE: process.env.MODE || "inbox", //private or public
+AUTO_VOICE:"false", //true or false
+MONGODB: "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
+CAPTION: "> *NADEEN-MD*",
+JIDS: [],
+SUDO: ['94766863255']
+
+};
